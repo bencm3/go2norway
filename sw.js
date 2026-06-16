9be540cd2283bb5d3cfbdd36af3e3662a80e149f
bencm3 <bencm3@gmail.com>
2026-06-16 09:07:13 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,30 +1,5 @@
-const CACHE = 'go2norway-v30';
-const ASSETS = [
-  './',
-  './index.html',
-  './index.html?v=30',
-  './Go2Norway_v30_highlights_rich_info.html',
-  './manifest.webmanifest',
-  './icon-192.png',
-  './icon-512.png',
-  './maskable-icon-512.png'
-];
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
-  self.skipWaiting();
-});
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
-  );
-});
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request).then(networkResp => {
-      const copy = networkResp.clone();
-      caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(()=>{});
-      return networkResp;
-    }).catch(() => caches.match(event.request).then(resp => resp || caches.match('./index.html')))
-  );
-});
+const CACHE='go2norway-v34';
+const ASSETS=['./','./index.html?v=34','./manifest.webmanifest','./icon-192.png','./icon-512.png','./maskable-icon-512.png','./app-icon-original.png','./Go2Norway_v34_kontakty_sirka_mapy.html?v=34'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return; e.respondWith(fetch(e.request).then(r=>{const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html?v=34'))))});
